cc18715ff10cd86d02ce16e73405556f584637de
Mr4x3 <Vivek@doyl.in>
2016-03-09 16:59:50 +0530

    Version 0.17

--- ChangeRevert.py
+++
@@ -1,4 +0,0 @@
-# create in sql changes revert back script
-class revertBack():
-    """Revert back script so that changes created by patch script can be revert"""
-    #with open
--- schemaobject/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-#author = 'Mitch'
-#Copyright 2009-2010 Mitch Matuson
-
-#shortcut to SchemaObject()
-from schemaobject.schema import SchemaObject
