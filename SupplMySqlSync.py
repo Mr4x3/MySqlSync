999d054ab7680d20fa225e9ee157afff0844007b
Mr4x3 <Vivek@doyl.in>
2016-03-10 17:23:07 +0530

    Version 0.22

--- SupplMySqlSync.py
+++ SupplMySqlSync.py
@@ -2,19 +2,27 @@
 #Hardcoded
 #Custom Hardcoded Wraapper for sql file created
 #this Will exclude some tables which are to be Excluded
+#Full Fleged
 
 import MySQLdb
 import sys
 import re
-
+import datetime
+import os
+import time
+from info import *
 r=sys.argv
-status=os.popen('python3 {} {} {}'.format(r[1],r[2]))
+if len(r)<3:
+    print("Usage python3 SupplMySqlSync.py [sourcedb] [destinationdb] \n Source and destination format mysql://username:password:@productionserver:3306/other")
+status=os.popen('python3 MySqlSync.py -a {} {}'.format(r[1],r[2]))
+time.sleep(5)
 #source,destination
-print("Usage $python3 queryFileTodb.py database.sql host user password database")
-z= sys.argv
+#mysql://username:password:@productionserver:3306/other
+#print("Usage $python3 queryFileTodb.py database.sql host user password database")
+z=r[2][r[2].rfind('/')+1:]+'.'+datetime.datetime.now().strftime(DATE_FORMAT)+'.'+"patch.sql"
 listOfExcludedTables=['django_','auth_']
 buffer=[]
-with open(z[1]) as sql:
+with open(z) as sql:
     for i in sql.readlines():
         if i.startswith("""CREATE TABLE `auth_""") or i.startswith("""CREATE TABLE `django_"""):
             continue
